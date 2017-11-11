d35625fd562e3e1a9104c8757df099964c2878e2
Eduardo Lacerda <lacerda.snr@gmail.com>
2017-11-11 12:42:05 -0200

    W1U3 -> Components

--- webapp/app.js
+++ webapp/app.js
@@ -5,12 +5,12 @@ var app = express();
 
 app.use('/view', express.static('./view'));
 app.use('/controller', express.static('./controller'));
+app.use(express.static('.'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Express server running on port 3000');
 });
-  
