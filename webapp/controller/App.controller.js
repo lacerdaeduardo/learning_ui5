0032dc3e6cc1b7de9ba677dea48a53fa1860247f
Lacerda Eduardo <lacerda.snr@gmail.com>
2017-11-16 19:07:54 +0000

    W2U4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
     'opensap/myapp/model/formatter'
-], function(Controller, MessageToast, formatter) {
+], function(Controller, MessageToast, Filter, FilterOperator, formatter) {
     'use strict';
     return Controller.extend('opensap.myapp.controller.App', {
     	formatter: formatter,
@@ -15,5 +17,22 @@ sap.ui.define([
 			// show message
 			MessageToast.show(sMsg);
         },
+		onFilterProducts : function (oEvent) {
+
+			// build filter array
+			var aFilter = [], sQuery = oEvent.getParameter("query"),
+				// retrieve list control
+				oList = this.getView().byId("productsList"),
+				// get binding for aggregation 'items'
+				oBinding = oList.getBinding("items");
+
+			if (sQuery) {
+				aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+			}
+			// apply filter. an empty filter array simply removes the filter
+			// which will make all entries visible again
+			oBinding.filter(aFilter);
+		}
+
     });
 });
